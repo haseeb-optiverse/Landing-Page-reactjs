b6cff1e7ac072475068725f569d72a9c83f51b35
haseeb-optiverse <haseebff049@gmail.com>
2026-02-07 12:39:44 +0500

    homePage

---
+++ home-page/src/assets/index.js
@@ -0,0 +1,7 @@
+export { default as Tick } from "./blue-tick.jpg";
+export { default as logo } from "./react.svg";
+export { default as image } from "./images.jpg";
+export { default as house } from "./house.jpg";
+export { default as search } from "./search-image.jpg";
+export { default as expert} from "./expert-support.jpg";
+export { default as world } from "./world-icon.png";
---
+++ home-page/src/components/static/static-data.jsx
@@ -0,0 +1,61 @@
+import { house } from "../../assets";
+
+
+ export const productCardData = [
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+  {
+    image: house,
+    title: "Islamabad Society",
+    pkr: "Pkr 2.88core to 230core",
+    address: "DHA,Phase 1,islamabad",
+    street: "Block G,Street no 2",
+  },
+];
