a9ffbaa5bdc4d79e6288e28fc03d54ce76d50a9a
Mateen Nawaz <mateenNawaz86>
2026-02-12 09:57:36 +0500

    fix: add feature as HTML

--- home-page/src/components/home-components/home-page-1/planCards.jsx
+++ home-page/src/components/home-components/home-page-1/planCards.jsx
@@ -1,24 +1,27 @@
-import { Plans } from "../static-data-2/static-data-2"
+import { Plans } from "../static-data-2/static-data-2";
 
 export const Choice = () => {
-    return(
-         <div className="max-w-6xl mx-auto px-4">
-        <div className="grid md:grid-cols-3 gap-6 p-4">
-          {Plans.map((plan, i) => (
-            <div
-              key={i}
-              className="bg-white rounded-xl p-6 text-center shadow-lg"
-            >
-              <h2 className="text-xl font-semibold">{plan.title}</h2>
-              <p className="text-sm opacity-70 mb-4">{plan.desc}</p>
-              <p className="text-lg  mb-4">{plan.features}</p>
+  return (
+    <div className="max-w-6xl mx-auto px-4">
+      <div className="grid md:grid-cols-3 gap-6 p-4">
+        {Plans.map((plan, i) => (
+          <div
+            key={i}
+            className="bg-white rounded-xl p-6 text-center shadow-lg"
+          >
+            <h2 className="text-xl font-semibold">{plan.title}</h2>
+            <p className="text-sm opacity-70 mb-4">{plan.desc}</p>
+            <p
+              className="text-lg mb-4"
+              dangerouslySetInnerHTML={{ __html: plan.features }}
+            />
 
-              <button className="px-4 py-2 rounded-md text-sm bg-orange-500 text-white">
-                {plan.button}
-              </button>
-            </div>
-          ))}
-        </div>
+            <button className="px-4 py-2 rounded-md text-sm bg-orange-500 text-white">
+              {plan.button}
+            </button>
+          </div>
+        ))}
       </div>
-    )
-}
+    </div>
+  );
+};
---
+++ home-page/src/components/home-components/static-data-2/static-data-2.js
@@ -0,0 +1,33 @@
+export const Plans = [
+  {
+    title: "Free Plan",
+    desc: "Perfect for individuals",
+    features: `
+    <p>Up to 10 active projects</p>
+    <p>Unlimited tasks & members</p>
+    <p>Basic reporting</p>
+        `,
+
+    button: "Choice plan",
+  },
+  {
+    title: "Pro Plan",
+    desc: "Perfect for individuals",
+    features: `
+    <p>Up to 10 active projects</p>
+    <p>Unlimited tasks & members</p>
+    <p>Basic reporting</p>
+        `,
+    button: "Choice plan",
+  },
+  {
+    title: "Master Plan",
+    desc: "Perfect for individuals",
+    features: `
+    <p>Up to 10 active projects</p>
+    <p>Unlimited tasks & members</p>
+    <p>Basic reporting</p>
+        `,
+    button: "Choice plan",
+  },
+];
